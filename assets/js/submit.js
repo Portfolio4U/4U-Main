5e980a77209d6bd56cf7086e1cb15b6ebd78ca11
Portfolio4U <163510429+Portfolio4U@users.noreply.github.com>
2024-03-19 16:51:43 +0530

    Update submit.js

--- assets/js/submit.js
+++ assets/js/submit.js
@@ -1,5 +1,4 @@
-
-    document.getElementById("phone").addEventListener("input", function(event) {
+document.getElementById("phone").addEventListener("input", function(event) {
         const phoneInput = event.target;
         const isValid = phoneInput.checkValidity();
 
@@ -10,3 +9,32 @@
         }
     });
 
+
+
+    function showPopup() {
+        document.getElementById("sharePopup").style.display = "block";
+      }
+      
+      // Function to close the popup
+      function closePopup() {
+        document.getElementById("sharePopup").style.display = "none";
+      }
+      
+      // Event listener for the share button
+      document.getElementById("shareButton").addEventListener("click", function() {
+        showPopup();
+      });
+
+      // Function to copy the link to clipboard
+
+      function copyLink() {
+        var link = "https://www.portfolio4u.online/";
+        navigator.clipboard.writeText(link).then(function() {
+          document.getElementById("copyStatus").style.display = "inline";
+          setTimeout(function() {
+            document.getElementById("copyStatus").style.display = "none";
+          }, 2000); // Hide after 2 seconds
+        }, function() {
+          alert("Failed to copy link.");
+        });
+      }
